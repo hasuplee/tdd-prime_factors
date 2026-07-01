6e22818da7830b5b9e120ab29e0478c54c26b2ae
hasup.lee <hasup.lee@samsung.com>
2026-07-01 13:00:56 +0900

    refactoring [5-2] divisor 추출

--- prime_factors.py
+++ prime_factors.py
@@ -2,18 +2,19 @@ class PrimeFactor:
     def of(self, num: int) -> list[int]:
         factors = []
         if num > 1:
+            divisor = 2
             if num == 4:
-                while num % 2 == 0:
-                    factors.append(2)
-                    num /= 2
+                while num % divisor == 0:
+                    factors.append(divisor)
+                    num /= divisor
             elif num == 6:
-                while num % 2 == 0:
-                    factors.append(2)
-                    num /= 2
-                while num % 3 == 0:
-                    factors.append(3)
-                    num /= 3
-                # factors.append(3)
+                while num % divisor == 0:
+                    factors.append(divisor)
+                    num /= divisor
+                divisor += 1
+                while (num % divisor == 0):
+                    factors.append(divisor)
+                    num /= divisor
             else:
                 factors.append(num)
 
