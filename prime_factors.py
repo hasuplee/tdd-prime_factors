a720106868eef19480a4210fb2508bc529be9a82
hasup.lee <hasup.lee@samsung.com>
2026-07-01 12:49:59 +0900

    refactoring [1] 2,3에 대해서 일반화

--- prime_factors.py
+++ prime_factors.py
@@ -2,8 +2,8 @@ class PrimeFactor:
     def of(self, num: int) -> list[int]:
         factors = []
         if num == 2:
-            factors.append(2)
+            factors.append(num)
         elif num == 3:
-            factors.append(3)
+            factors.append(num)
 
         return factors
